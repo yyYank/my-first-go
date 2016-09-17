1d3e978118591090c89506e99de7b93e9373c915
yy_yank <yy.yank.dev.blog@gmail.com>
2016-09-17 18:30:00 +0900

    create echo command

---
+++ src/com/github/yyYank/echo/Echo.go
@@ -0,0 +1,38 @@
+/*
+  http://golang.jp/go_tutorial#index04
+*/
+package main
+
+import (
+	"os"
+	"flag"
+)
+
+
+var omitNewLine = flag.Bool("n", false, "don't print final newline")
+
+
+const (
+	Space = ""
+	Newline = "\n"
+)
+
+func echo() {
+	//  パラメータリストをフラグに設定
+	flag.Parse()
+	var s string = ""
+	for i:= 0; i < flag.NArg(); i++ {
+		if i > 0 {
+			s += Space
+		}
+		s += flag.Arg(i)
+	}
+
+
+	if !*omitNewLine {
+		s+= Newline
+	}
+
+	// OSの標準出力へ書き込み
+	os.Stdout.WriteString(s)
+}
