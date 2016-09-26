67844ff4eb1a29294b64bf7e25fd17f42a60c8a5
yy_yank <yy.yank.dev.blog@gmail.com>
2016-09-26 22:17:53 +0900

    modify

--- src/blogfetch/blogfetch.go
+++ src/blogfetch/blogfetch.go
@@ -4,12 +4,33 @@ import (
 	"net/http"
 	"io/ioutil"
 	"fmt"
+	"encoding/xml"
+	"strconv"
 )
 
 func main() {
-	url := "http://yyyank.blogspot.com/feeds/posts/default?max-results=1"
+	url := "http://yyyank.blogspot.com/feeds/posts/default?max-results=999"
 	response, _ := http.Get(url)
 	defer response.Body.Close()
-	byteArray, _ := ioutil.ReadAll(response.Body)
-	fmt.Println(string(byteArray)) // htmlをstringで取得
+	byteArr, _ := ioutil.ReadAll(response.Body)
+	v := Result{}
+	err := xml.Unmarshal(byteArr, &v)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+	for i := range v.Entries {
+		fmt.Printf("Title: %q\n", v.Entries[i])
+	}
+	fmt.Println("ブログの投稿数=====>>>" + strconv.Itoa(len(v.Entries)))
+}
+
+type Result struct {
+	Entries []Entry `xml:"entry"`
+}
+type Entry struct {
+	Titles []Title `xml:"title"`
+}
+type Title struct {
+	Title string `xml:",chardata"`
 }
