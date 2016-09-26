a329992e2c9dd4afca04689b5e733c947fea2414
yy_yank <yy.yank.dev.blog@gmail.com>
2016-09-26 22:23:42 +0900

    add ref url

--- src/blogfetch/blogfetch.go
+++ src/blogfetch/blogfetch.go
@@ -8,6 +8,12 @@ import (
 	"strconv"
 )
 
+/*
+このあたりを参考にした
+ http://qiita.com/taizo/items/c397dbfed7215969b0a5
+ http://cuto.unirita.co.jp/gostudy/post/standard-library-xml/
+ */
+
 func main() {
 	url := "http://yyyank.blogspot.com/feeds/posts/default?max-results=999"
 	response, _ := http.Get(url)
